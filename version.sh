c11cfbcd2715f8a67ff352bbd09cae969e264924
ahoka <ahodesuka@users.noreply.github.com>
2015-06-21 07:18:31 -0500

    Add version.sh used for creating releases

---
+++ version.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ -n "$1" ]
+then
+    VERSION="$1"
+elif [ -d '.git' ]
+then
+    git update-index --refresh > /dev/null 2>&1
+    VERSION=$(git describe --abbrev=4 --match='[0-9]*' --dirty 2>/dev/null)
+fi
+
+[ -n "$VERSION" ] && echo -n "$VERSION" > VERSION
+
+if [ -f "VERSION" ]
+then
+    VERSION=$(cat VERSION)
+else
+    VERSION="UNKNOWN"
+fi
+
+cat <<EOF > src/version.h
+#ifndef _VERSION_
+#define _VERSION_ "$VERSION"
+#endif
+EOF
+
+echo $VERSION
