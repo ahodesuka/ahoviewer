928eb82ccb4ce43db374511ff399c395b2efc45e
ahoka <ahodesuka@users.noreply.github.com>
2015-08-11 17:53:40 -0500

    version.sh: Update version.h even if the version is unknown

--- version.sh
+++ version.sh
@@ -20,7 +20,7 @@ else
     VERSION="UNKNOWN"
 fi
 
-if [ $VERSION != $OLD_VERSION -a $VERSION != "UNKNOWN" -o ! -f "src/version.h" ]
+if [ $VERSION != $OLD_VERSION -o ! -f "src/version.h" ]
 then
     cat <<EOF > src/version.h
 #define AHOVIEWER_VERSION "$VERSION"
