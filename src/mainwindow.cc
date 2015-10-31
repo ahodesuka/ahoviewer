6e625f01a92d4b213ab79102f77cbb24c6f8eb98
ahoka <ahodesuka@users.noreply.github.com>
2015-10-31 06:24:22 -0500

    mainwindow: Add license to about dialog

--- src/mainwindow.cc
+++ src/mainwindow.cc
@@ -108,6 +108,26 @@ MainWindow::MainWindow(BaseObjectType *cobj, const Glib::RefPtr<Gtk::Builder> &b
     m_AboutDialog->set_version(ahoviewer_version);
     m_AboutDialog->set_copyright("Copyright \302\251 2013-2015 ahoka");
     m_AboutDialog->set_website(PACKAGE_URL);
+
+    m_AboutDialog->set_license(
+        "Copyright (c) 2013-2015 ahoka\n"
+        "\n"
+        "Permission is hereby granted, free of charge, to any person obtaining a copy of\n"
+        "this software and associated documentation files (the \"Software\"), to deal in\n"
+        "the Software without restriction, including without limitation the rights to\n"
+        "use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of\n"
+        "the Software, and to permit persons to whom the Software is furnished to do so,\n"
+        "subject to the following conditions:\n"
+        "\n"
+        "The above copyright notice and this permission notice shall be included in all\n"
+        "copies or substantial portions of the Software.\n"
+        "\n"
+        "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+        "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS\n"
+        "FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR\n"
+        "COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER\n"
+        "IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN\n"
+        "CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.");
     // }}}
 
     // Setup drag and drop
