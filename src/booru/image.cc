bff1c1d84ffee2dcf84f79618a6d05ab986d5e2c
ahoka <ahodesuka@users.noreply.github.com>
2015-11-02 20:13:52 -0600

    booru/image: Thumbnail curlers do not need to be class members

--- src/booru/image.cc
+++ src/booru/image.cc
@@ -17,7 +17,6 @@ Image::Image(const std::string &path, const std::string &url,
     m_Tags(tags),
     m_Page(page),
     m_Curler(m_Url),
-    m_ThumbnailCurler(m_ThumbnailUrl),
     m_PixbufError(false)
 {
     m_ThumbnailPath = thumbPath;
@@ -47,10 +46,12 @@ const Glib::RefPtr<Gdk::Pixbuf>& Image::get_thumbnail()
 {
     if (!m_ThumbnailPixbuf)
     {
+        Curler curler(m_ThumbnailUrl);
+
         m_ThumbnailLock.writer_lock();
-        if (m_ThumbnailCurler.perform())
+        if (curler.perform())
         {
-            m_ThumbnailCurler.save_file(m_ThumbnailPath);
+            curler.save_file(m_ThumbnailPath);
 
             try
             {
@@ -65,7 +66,7 @@ const Glib::RefPtr<Gdk::Pixbuf>& Image::get_thumbnail()
         else
         {
             std::cerr << "Error while downloading thumbnail " << m_ThumbnailUrl
-                      << " " << std::endl << "  " << m_ThumbnailCurler.get_error() << std::endl;
+                      << " " << std::endl << "  " << curler.get_error() << std::endl;
             m_ThumbnailPixbuf = get_missing_pixbuf();
         }
         m_ThumbnailLock.writer_unlock();
