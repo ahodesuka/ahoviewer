ahodesuka/ahoviewer

commit e31209996246c8331dbb6ca2bb9c6acf50f47aad

booru/page: Store current tags when executing a search
This fixes some odd behavior when the user would search and then change
the tags.  Loading new pages of the original search would reflect the
new tags - without the user having to submit the new tags.
Also made it so when a user enters no tags the tags are set to *.

src/booru/page.cc
@@ -137,18 +137,21 @@ void Page::search(const std::shared_ptr<Site> &site)
     m_Site = site;
     m_Page = 1;
     m_LastPage = false;
-
-    std::string tags = m_Tags;
+    m_SearchTags = m_Tags;
 
     // Trim whitespace for the tab label text
-    if (!tags.empty())
+    if (!m_SearchTags.empty())
     {
-        size_t f = tags.find_first_not_of(' '),
-               l = tags.find_last_not_of(' ');
-        tags = f == std::string::npos ? "" : " - " + tags.substr(f, l - f + 1);
+        size_t f = m_SearchTags.find_first_not_of(' '),
+               l = m_SearchTags.find_last_not_of(' ');
+        m_SearchTags = f == std::string::npos ? "" : " - " + m_SearchTags.substr(f, l - f + 1);
+    }
+    else
+    {
+        m_SearchTags = "*";
     }
 
-    m_TabLabel->set_text(m_Site->get_name() + tags);
+    m_TabLabel->set_text(m_Site->get_name() + m_SearchTags);
     m_TabIcon->set(m_Site->get_icon_pixbuf());
 
     m_Curler.set_referer(m_Site->get_url());

src/booru/page.h
@@ -70,7 +70,7 @@ namespace AhoViewer
             std::shared_ptr<Site> m_Site;
             Curler m_Curler, m_CountsCurler;
 
-            std::string m_Tags, m_Path;
+            std::string m_Tags, m_SearchTags, m_Path;
             size_t m_Page, m_NumPosts,
                    m_SaveImagesTotal;
             std::atomic<size_t> m_SaveImagesCurrent;

commit 69a54d1818df80177cb9020f41118af5d0b15054

booru/page: Fix issues with tags introduced in e312099

src/booru/page.cc
@@ -139,19 +139,20 @@ void Page::search(const std::shared_ptr<Site> &site)
     m_LastPage = false;
     m_SearchTags = m_Tags;
 
+    std::string tags = m_Tags;
+
     // Trim whitespace for the tab label text
-    if (!m_SearchTags.empty())
+    if (!tags.empty())
     {
-        size_t f = m_SearchTags.find_first_not_of(' '),
-               l = m_SearchTags.find_last_not_of(' ');
-        m_SearchTags = f == std::string::npos ? "" : " - " + m_SearchTags.substr(f, l - f + 1);
-    }
-    else
-    {
-        m_SearchTags = "*";
+        size_t f = tags.find_first_not_of(' '),
+               l = tags.find_last_not_of(' ');
+        tags = f == std::string::npos ? "" : " - " + tags.substr(f, l - f + 1);
     }
 
-    m_TabLabel->set_text(m_Site->get_name() + m_SearchTags);
+    if (m_SearchTags.empty())
+        m_SearchTags = "*";
+
+    m_TabLabel->set_text(m_Site->get_name() + tags);
     m_TabIcon->set(m_Site->get_icon_pixbuf());
 
     m_Curler.set_referer(m_Site->get_url());