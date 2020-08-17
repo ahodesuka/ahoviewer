6a5b1118dd28b16a481ffb1614ed48b3d2b42cc8
ahoka <ahodesuka@users.noreply.github.com>
2020-08-17 13:20:46 -0500

    mainwindow: Use GTK's built in license type in the about dialog

--- src/mainwindow.cc
+++ src/mainwindow.cc
@@ -147,26 +147,6 @@ MainWindow::MainWindow(BaseObjectType* cobj, Glib::RefPtr<Gtk::Builder> bldr)
                                               "HaxtonFale",
                                               "WebFreak001",
                                           });
-
-        m_AboutDialog->set_license(
-            "Copyright (c) 2013-2020 ahoka\n"
-            "\n"
-            "Permission is hereby granted, free of charge, to any person obtaining a copy of\n"
-            "this software and associated documentation files (the \"Software\"), to deal in\n"
-            "the Software without restriction, including without limitation the rights to\n"
-            "use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of\n"
-            "the Software, and to permit persons to whom the Software is furnished to do so,\n"
-            "subject to the following conditions:\n"
-            "\n"
-            "The above copyright notice and this permission notice shall be included in all\n"
-            "copies or substantial portions of the Software.\n"
-            "\n"
-            "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
-            "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS\n"
-            "FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR\n"
-            "COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER\n"
-            "IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN\n"
-            "CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.");
     }
     // }}}
 
@@ -1158,7 +1138,7 @@ void MainWindow::on_show_about()
         m_AboutDialog->hide();
 
     m_AboutDialog->set_transient_for(*this);
-    m_AboutDialog->show_all();
+    m_AboutDialog->show();
 }
 
 void MainWindow::on_open_recent_file()
