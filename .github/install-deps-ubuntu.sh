df17e410633c9da65cf112465c02d06f4eb2171f
ahoka <ahodesuka@users.noreply.github.com>
2022-09-11 03:11:26 -0500

    github: Use ubuntu 22.04 for ci builds

--- .github/install-deps-ubuntu.sh
+++ .github/install-deps-ubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 sudo apt-get update
-sudo apt-get install -qy libgtkmm-3.0-dev \
+sudo apt remove -y libunwind-14-dev
+sudo apt-get install -qy meson \
+    libgtkmm-3.0-dev \
     libconfig++-dev \
     libcurl4-openssl-dev \
     libxml2-dev \
